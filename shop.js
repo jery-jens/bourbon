eb808f92b39407a6bc50e5d54a7d9e98ce5feee5
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-28 16:24:23 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -64,6 +64,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", (e) => {
-        window.location = window.location.pathname + "/out-of-stock";
+        window.location = window.location.pathname.replace("shot", "out-of-stock");
     });
 });
