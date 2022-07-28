1e637aa76ebc847e2667921035ce36ea91ff09b9
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-28 20:57:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const DOMAINS = {
         "everclean": "/",
+        "cleanaround": "/",
+        "360clean": "/",
+        "perpetua": "/",
     };
 
     const SLUG_ID = {
         "everclean": "ec",
+        "cleanaround": "ca",
+        "360clean": "360clean",
+        "perpetua": "perpetua",
     };
 
-    const slugId = window.location.pathname.split("/")[0];
+    const slugId = window.location.pathname.split("/");
     console.log(slugId);
 });
