2c12ddd5af04d24c0a14ca8704730133cd293a78
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-28 14:55:22 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -42,12 +42,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     amount.innerHTML = Number(amount.innerHTML) - 1;
                     Number(amount.innerHTML) === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
                     price = price - productPrice;
-                    document.querySelector(".price").innerHTML = `$${price.toFixed(2)}`;
+                    document.querySelector(".price").innerHTML = `£${price.toFixed(2)}`;
                     document.getElementById(productName).value = Number(amount.innerHTML);
-                };
-
-                if (index === 0) {
-                    Number(amount.innerHTML) > 0 ? innerBoxes[1].classList.remove("closed") : innerBoxes[1].classList.add("closed");
                 };
             });
 
@@ -55,12 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 amount.innerHTML = Number(amount.innerHTML) + 1;
                 Number(amount.innerHTML) === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
                 price = price + productPrice;
-                document.querySelector(".price").innerHTML = `$${price.toFixed(2)}`;
+                document.querySelector(".price").innerHTML = `£{price.toFixed(2)}`;
                 document.getElementById(productName).value = Number(amount.innerHTML);
-
-                if (index === 0) {
-                    Number(amount.innerHTML) > 0 ? innerBoxes[1].classList.remove("closed") : innerBoxes[1].classList.add("closed");
-                };
             });
         });
     });
