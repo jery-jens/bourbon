f2eafcf7f7dc84a56e93de0b74fad18051624b38
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-29 11:31:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
      */
 
     const DOMAINS = {
-        "everclean": "/",
-        "cleanaround": "/",
-        "360clean": "/",
-        "perpetua": "/",
+        "everclean": "https://everclean-kitchen.com",
+        "cleanaround": "/https://clean-around-kitchen.com",
+        "360clean": "https://360-clean-kitchen.com",
+        "perpetua": "https://perpetua-kitchen.com",
     };
 
     const SLUG_ID = {
@@ -19,13 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const slugId = window.location.pathname.split("/")[1];
     
-    // if (slugId === SLUG_ID["360clean"]) {
-    //    window.location = DOMAINS["360clean"] + window.location.pathname;
-    // } else if (slugId === SLUG_ID.cleanaround) {
-    //     window.location = DOMAINS.cleanaround + window.location.pathname;
-    // } else if (slugId === SLUG_ID.everclean) {
-    //     window.location = DOMAINS.everclean + window.location.pathname;
-    // } else if (slugId === SLUG_ID.perpetua) {
-    //     window.location = DOMAINS.perpetua + window.location.pathname;
-    // };
+    if (slugId === SLUG_ID["360clean"]) {
+        console.log(DOMAINS["360clean"] + window.location.pathname)
+        //    window.location = DOMAINS["360clean"] + window.location.pathname;
+    } else if (slugId === SLUG_ID.cleanaround) {
+        // window.location = DOMAINS.cleanaround + window.location.pathname;
+    } else if (slugId === SLUG_ID.everclean) {
+        // window.location = DOMAINS.everclean + window.location.pathname;
+    } else if (slugId === SLUG_ID.perpetua) {
+        // window.location = DOMAINS.perpetua + window.location.pathname;
+    };
 });
