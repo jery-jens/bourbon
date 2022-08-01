7b91a347e681ab9036db3697b8fa2e001c388748
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-01 16:24:05 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -55,27 +55,26 @@ document.addEventListener("DOMContentLoaded", () => {
                             case 0:
                                 add = 0;
                                 takeOff = (productPrice / 100) * 100;
-                                innerBox.querySelector(".discount-label").style.display = "none";
                                 break;
                             case 1:
                                 add = (productPrice / 100) * 100;
                                 takeOff = ((productPrice * 2) / 100) * 90;
-                                innerBox.querySelector(".discount-label").innerHTML = "10% off";
+                                innerBox.querySelector(".discount-label").style.display = "none";
                                 break;
                             case 2:
                                 add = ((productPrice * 2) / 100) * 90;
                                 takeOff = ((productPrice * 3) / 100) * 87.5;
-                                innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
+                                innerBox.querySelector(".discount-label").innerHTML = "10% off";
                                 break;
                             case 3:
                                 add = ((productPrice * 3) / 100) * 87.5;
                                 takeOff = ((productPrice * 4) / 100) * 85;
-                                innerBox.querySelector(".discount-label").innerHTML = "15% off";
+                                innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
                                 break;
                             case 4:
                                 add = ((productPrice * 4) / 100) * 85;
                                 takeOff = ((productPrice * 5) / 100) * 80;
-                                innerBox.querySelector(".discount-label").innerHTML = "20% off";
+                                innerBox.querySelector(".discount-label").innerHTML = "15% off";
                                 break;
                             default:
                                 takeOff = (productPrice / 100) * 100
