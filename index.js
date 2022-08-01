7a826b68a58c2770c9ad7896dc201983c4757447
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-01 16:55:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "everclean": "www.everclean-kitchen.com",
         "cleanaround": "www.clean-around-kitchen.com",
         "360clean": "www.360-clean-kitchen.com",
-        "perpetua": "www.perpetua-kitchen.com",
+        "perpetua": "www.reveal-it-kitchen.com",
     };
 
     const SLUG_ID = {
@@ -19,23 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const slugId = window.location.pathname.split("/")[1];
     
-    // if (slugId === SLUG_ID["360clean"] && window.location.hostname !== DOMAINS["360clean"]) {
-    //     window.location = "https://" + DOMAINS["360clean"] + window.location.pathname;
-    // } else if (slugId === SLUG_ID.cleanaround && window.location.hostname !== DOMAINS.cleanaround) {
-    //     window.location = "https://" + DOMAINS.cleanaround + window.location.pathname;
-    // } else if (slugId === SLUG_ID.everclean && window.location.hostname !== DOMAINS.everclean) {
-    //     window.location = "https://" + DOMAINS.everclean+ window.location.pathname;
-    // } else if (slugId === SLUG_ID.perpetua && window.location.hostname !== DOMAINS.perpetua) {
-    //     // window.location = "https://" + DOMAINS.perpetua + window.location.pathname;
-    // };
+    if (slugId === SLUG_ID["360clean"] && window.location.hostname !== DOMAINS["360clean"]) {
+        window.location = "https://" + DOMAINS["360clean"] + window.location.pathname;
+    } else if (slugId === SLUG_ID.cleanaround && window.location.hostname !== DOMAINS.cleanaround) {
+        window.location = "https://" + DOMAINS.cleanaround + window.location.pathname;
+    } else if (slugId === SLUG_ID.everclean && window.location.hostname !== DOMAINS.everclean) {
+        window.location = "https://" + DOMAINS.everclean+ window.location.pathname;
+    } else if (slugId === SLUG_ID.perpetua && window.location.hostname !== DOMAINS.perpetua) {
+        window.location = "https://" + DOMAINS.perpetua + window.location.pathname;
+    };
 
-    // if (slugId !== SLUG_ID["360clean"] && window.location.hostname === DOMAINS["360clean"]) {
-    //     window.location = "https://" + window.location.hostname + "/" + SLUG_ID["360clean"] + "/landing-1";
-    // } else if (slugId !== SLUG_ID.cleanaround && window.location.hostname === DOMAINS.cleanaround) {
-    //     window.location = "https://" + window.location.hostname + "/" + SLUG_ID.cleanaround + "/landing-1";
-    // } else if (slugId !== SLUG_ID.everclean && window.location.hostname === DOMAINS.everclean) {
-    //     window.location = "https://" + window.location.hostname + "/" + SLUG_ID.everclean + "/landing-1";
-    // } else if (slugId !== SLUG_ID.perpetua && window.location.hostname === DOMAINS.perpetua) {
-    //     // window.location = "https://" + window.location.hostname + "/" + SLUG_ID.perpetua + "/landing-1";
-    // };
+    if (slugId !== SLUG_ID["360clean"] && window.location.hostname === DOMAINS["360clean"]) {
+        window.location = "https://" + window.location.hostname + "/" + SLUG_ID["360clean"] + "/landing-1";
+    } else if (slugId !== SLUG_ID.cleanaround && window.location.hostname === DOMAINS.cleanaround) {
+        window.location = "https://" + window.location.hostname + "/" + SLUG_ID.cleanaround + "/landing-1";
+    } else if (slugId !== SLUG_ID.everclean && window.location.hostname === DOMAINS.everclean) {
+        window.location = "https://" + window.location.hostname + "/" + SLUG_ID.everclean + "/landing-1";
+    } else if (slugId !== SLUG_ID.perpetua && window.location.hostname === DOMAINS.perpetua) {
+        window.location = "https://" + window.location.hostname + "/" + SLUG_ID.perpetua + "/landing-1";
+    };
 });  
