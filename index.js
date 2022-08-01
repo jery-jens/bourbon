efbb6d9c3be6b186ae293a8760269fc49740ce8c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-01 16:31:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (slugId === SLUG_ID.everclean && window.location.hostname !== DOMAINS.everclean) {
         window.location = "https://" + DOMAINS.everclean+ window.location.pathname;
     } else if (slugId === SLUG_ID.perpetua && window.location.hostname !== DOMAINS.perpetua) {
-        window.location = "https://" + DOMAINS.perpetua + window.location.pathname;
+        // window.location = "https://" + DOMAINS.perpetua + window.location.pathname;
     };
 
     if (slugId !== SLUG_ID["360clean"] && window.location.hostname === DOMAINS["360clean"]) {
@@ -36,6 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (slugId !== SLUG_ID.everclean && window.location.hostname === DOMAINS.everclean) {
         window.location = "https://" + window.location.hostname + "/" + SLUG_ID.everclean + "/landing-1";
     } else if (slugId !== SLUG_ID.perpetua && window.location.hostname === DOMAINS.perpetua) {
-        window.location = "https://" + window.location.hostname + "/" + SLUG_ID.perpetua + "/landing-1";
+        // window.location = "https://" + window.location.hostname + "/" + SLUG_ID.perpetua + "/landing-1";
     };
 });  
