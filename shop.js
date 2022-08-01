97567fbef3591682161b1355b538a76c967600d6
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-01 16:08:14 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -41,122 +41,122 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const hasDiscount = Boolean(document.getElementById(productName).getAttribute("data-discount")) ?? false;
 
-            minus.addEventListener("click", () => {
-                if (productAmount !== 0) {
-                    productAmount = productAmount - 1;
-                    amount.innerHTML = productAmount;
-                    productAmount === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
-                    plus.classList.remove("not-active");
+        //     minus.addEventListener("click", () => {
+        //         if (productAmount !== 0) {
+        //             productAmount = productAmount - 1;
+        //             amount.innerHTML = productAmount;
+        //             productAmount === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
+        //             plus.classList.remove("not-active");
 
-                    if (hasDiscount) {
-                        let takeOff = 0;
-                        let add = 0;
+        //             if (hasDiscount) {
+        //                 let takeOff = 0;
+        //                 let add = 0;
                         
-                        switch (productAmount) {
-                            case 0:
-                                add = 0;
-                                takeOff = (productPrice / 100) * 100;
-                                innerBox.querySelector(".discount-label").style.display = "none";
-                                break;
-                            case 1:
-                                add = (productPrice / 100) * 100;
-                                takeOff = ((productPrice * 2) / 100) * 90;
-                                innerBox.querySelector(".discount-label").innerHTML = "10% off";
-                                break;
-                            case 2:
-                                add = ((productPrice * 2) / 100) * 90;
-                                takeOff = ((productPrice * 3) / 100) * 87.5;
-                                innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
-                                break;
-                            case 3:
-                                add = ((productPrice * 3) / 100) * 87.5;
-                                takeOff = ((productPrice * 4) / 100) * 85;
-                                innerBox.querySelector(".discount-label").innerHTML = "15% off";
-                                break;
-                            case 4:
-                                add = ((productPrice * 4) / 100) * 85;
-                                takeOff = ((productPrice * 5) / 100) * 80;
-                                innerBox.querySelector(".discount-label").innerHTML = "20% off";
-                                break;
-                            default:
-                                takeOff = (productPrice / 100) * 100
-                                break;
-                        }
+        //                 switch (productAmount) {
+        //                     case 0:
+        //                         add = 0;
+        //                         takeOff = (productPrice / 100) * 100;
+        //                         innerBox.querySelector(".discount-label").style.display = "none";
+        //                         break;
+        //                     case 1:
+        //                         add = (productPrice / 100) * 100;
+        //                         takeOff = ((productPrice * 2) / 100) * 90;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "10% off";
+        //                         break;
+        //                     case 2:
+        //                         add = ((productPrice * 2) / 100) * 90;
+        //                         takeOff = ((productPrice * 3) / 100) * 87.5;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
+        //                         break;
+        //                     case 3:
+        //                         add = ((productPrice * 3) / 100) * 87.5;
+        //                         takeOff = ((productPrice * 4) / 100) * 85;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "15% off";
+        //                         break;
+        //                     case 4:
+        //                         add = ((productPrice * 4) / 100) * 85;
+        //                         takeOff = ((productPrice * 5) / 100) * 80;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "20% off";
+        //                         break;
+        //                     default:
+        //                         takeOff = (productPrice / 100) * 100
+        //                         break;
+        //                 }
 
-                        price = (price - takeOff) + add;   
-                    } else {
-                        price = price - productPrice;
-                    };
+        //                 price = (price - takeOff) + add;   
+        //             } else {
+        //                 price = price - productPrice;
+        //             };
 
-                    document.querySelector(".price").innerHTML = `£${price.toFixed(2)}`;
-                    document.getElementById(productName).value = productAmount;
-                };
-            });
+        //             document.querySelector(".price").innerHTML = `£${price.toFixed(2)}`;
+        //             document.getElementById(productName).value = productAmount;
+        //         };
+        //     });
 
-            plus.addEventListener("click", () => {
-                productAmount = productAmount + 1;
+        //     plus.addEventListener("click", () => {
+        //         productAmount = productAmount + 1;
 
-                if (productAmount === 5) {
-                    amount.innerHTML = productAmount;
-                    plus.classList.add("not-active");
+        //         if (productAmount === 5) {
+        //             amount.innerHTML = productAmount;
+        //             plus.classList.add("not-active");
 
-                    if (hasDiscount) {
-                        let takeOff = ((productPrice * 4) / 100) * 85;
-                        let add = ((productPrice * 5) / 100) * 80;
+        //             if (hasDiscount) {
+        //                 let takeOff = ((productPrice * 4) / 100) * 85;
+        //                 let add = ((productPrice * 5) / 100) * 80;
     
-                        price = (price - takeOff) + add;
-                    } else {
-                        price = price + productPrice;
-                    };
-                } else if (productAmount < 5) {
-                    amount.innerHTML = productAmount;
-                    productAmount === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
+        //                 price = (price - takeOff) + add;
+        //             } else {
+        //                 price = price + productPrice;
+        //             };
+        //         } else if (productAmount < 5) {
+        //             amount.innerHTML = productAmount;
+        //             productAmount === 0 ? minus.classList.add("not-active") : minus.classList.remove("not-active");
 
-                    if (hasDiscount) {
-                        let takeOff = 0;
-                        let add = 0;
+        //             if (hasDiscount) {
+        //                 let takeOff = 0;
+        //                 let add = 0;
                         
-                        switch (productAmount) {
-                            case 1:
-                                takeOff = 0;
-                                add = (productPrice / 100) * 100;
-                                innerBox.querySelector(".discount-label").style.display = "none";
-                                break;
-                            case 2:
-                                takeOff = (productPrice / 100) * 100;
-                                add = ((productPrice * 2) / 100) * 90;
-                                innerBox.querySelector(".discount-label").style.display = "block";
-                                innerBox.querySelector(".discount-label").innerHTML = "10% off";
-                                break;
-                            case 3:
-                                takeOff = ((productPrice * 2) / 100) * 90;
-                                add = ((productPrice * 3) / 100) * 87.5;
-                                innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
-                                break;
-                            case 4:
-                                takeOff = ((productPrice * 3) / 100) * 87.5;
-                                add = ((productPrice * 4) / 100) * 85;
-                                innerBox.querySelector(".discount-label").innerHTML = "15% off";
-                                break;
-                            case 5:
-                                takeOff = ((productPrice * 4) / 100) * 85;
-                                add = ((productPrice * 5) / 100) * 80;
-                                innerBox.querySelector(".discount-label").innerHTML = "20% off";
-                                break;
-                            default:
-                                add = (productPrice / 100) * 100
-                                break;
-                        }
+        //                 switch (productAmount) {
+        //                     case 1:
+        //                         takeOff = 0;
+        //                         add = (productPrice / 100) * 100;
+        //                         innerBox.querySelector(".discount-label").style.display = "none";
+        //                         break;
+        //                     case 2:
+        //                         takeOff = (productPrice / 100) * 100;
+        //                         add = ((productPrice * 2) / 100) * 90;
+        //                         innerBox.querySelector(".discount-label").style.display = "block";
+        //                         innerBox.querySelector(".discount-label").innerHTML = "10% off";
+        //                         break;
+        //                     case 3:
+        //                         takeOff = ((productPrice * 2) / 100) * 90;
+        //                         add = ((productPrice * 3) / 100) * 87.5;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "12.5% off";
+        //                         break;
+        //                     case 4:
+        //                         takeOff = ((productPrice * 3) / 100) * 87.5;
+        //                         add = ((productPrice * 4) / 100) * 85;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "15% off";
+        //                         break;
+        //                     case 5:
+        //                         takeOff = ((productPrice * 4) / 100) * 85;
+        //                         add = ((productPrice * 5) / 100) * 80;
+        //                         innerBox.querySelector(".discount-label").innerHTML = "20% off";
+        //                         break;
+        //                     default:
+        //                         add = (productPrice / 100) * 100
+        //                         break;
+        //                 }
 
-                        price = (price - takeOff) + add;
-                    } else {
-                        price = price + productPrice;
-                    };
-                };
+        //                 price = (price - takeOff) + add;
+        //             } else {
+        //                 price = price + productPrice;
+        //             };
+        //         };
 
-                document.querySelector(".price").innerHTML = `£${price.toFixed(2)}`;
-                document.getElementById(productName).value = productAmount;
-            });
+        //         document.querySelector(".price").innerHTML = `£${price.toFixed(2)}`;
+        //         document.getElementById(productName).value = productAmount;
+        //     });
         });
     });
 
